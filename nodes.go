da4474588fd8db8884ae99b4b9f8f6b51f3d0055
Chris Wacek <cwacek@gmail.com>
2013-12-18 09:09:07 -0500

    Added support for querying facts.

--- nodes.go
+++ nodes.go
@@ -5,11 +5,35 @@ import "time"
 type NodeList []*Node
 
 type Node struct {
-  Name string
-  Active bool
-  Deactivated *time.Time
-  CatalogTime *time.Time `json:"catalog_timestamp"`
-  FactsTime *time.Time `json:"facts_timestamp"`
-  ReportedTime *time.Time `json:"reported_timestamp"`
+	Name         string
+	Active       bool
+	Deactivated  *time.Time
+	CatalogTime  *time.Time `json:"catalog_timestamp"`
+	FactsTime    *time.Time `json:"facts_timestamp"`
+	ReportedTime *time.Time `json:"reported_timestamp"`
 }
 
+type FactSet map[string]string
+
+/*// Retrieve a list of facts for a given node*/
+func (n *Node) Facts() (FactSet, error) {
+
+	facts := make([]*Fact, 0)
+	err := api_GET(&facts, "/v3/nodes/"+n.Name+"/facts")
+	if err != nil {
+		return nil, err
+	}
+
+	fact_dict := make(map[string]string)
+	for _, fact := range facts {
+		fact_dict[fact.Key] = fact.Value
+	}
+
+	return fact_dict, nil
+}
+
+type Fact struct {
+	Node  string `json:"certname"`
+	Key   string `json:"name"`
+	Value string
+}
--- puppetdb.go
+++ puppetdb.go
@@ -6,50 +6,61 @@ import "encoding/json"
 import "errors"
 
 var (
-  uri string
-  client *http.Client
+	base_uri string
+	client   *http.Client
 )
 
 // Configure a DBService to talk to the PuppetDB
 // database
 func Configure(host string, port int) {
-  uri = fmt.Sprintf("http://%s:%d", host, port)
-  client = &http.Client{}
+	base_uri = fmt.Sprintf("http://%s:%d", host, port)
+	client = &http.Client{}
 }
 
+// Make a request to uri and decode the response into
+// obj.
+func api_GET(obj interface{}, uri string) (err error) {
+
+	var (
+		req  *http.Request
+		resp *http.Response
+	)
+
+	req, err = http.NewRequest("GET", base_uri+uri, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Accept", "application/json")
+
+	resp, err = client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return errors.New(resp.Status)
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&obj)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Return a list of all available nodes
 func List() ([]*Node, error) {
 
-  var (
-  err error
-  req *http.Request
-  resp *http.Response
-)
+	nodes := make([]*Node, 0)
 
-  req, err = http.NewRequest("GET", uri + "/v3/nodes", nil)
-  if err != nil {
-    return nil, err
-  }
+	err := api_GET(&nodes, "/v3/nodes")
+	if err != nil {
+		return nil, err
+	}
 
-  req.Header.Add("Accept", "application/json")
-
-  resp, err = client.Do(req)
-  if err != nil {
-    return nil, err
-  }
-  defer resp.Body.Close()
-
-  if resp.StatusCode != 200 {
-    return nil, errors.New(resp.Status)
-  }
-
-  nodes := make([]*Node, 0)
-  decoder := json.NewDecoder(resp.Body)
-  err = decoder.Decode(&nodes)
-  if err != nil {
-    return nil, err
-  }
-
-  return nodes, nil
+	return nodes, nil
 }
